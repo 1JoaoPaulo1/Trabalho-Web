26ce3c2fc8e37bce48d7500fb5298ddd12e1188c
1JoaoPaulo1 <1joapaulo000@gmail.com>
2025-09-14 15:57:42 -0300

    O princípio
    Foram criadas todas as tabelas de banco de dados que serão utilizadas até o momento. Para poder ser feito um sistema de login, busca, leitura, notas e retenção de login.

---
+++ Lista_de_Leitura/controle/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from controle.models import Pessoa,Livro,Leitura,Nota,Logado
+# Register your models here.
+admin.site.register(Pessoa)
+admin.site.register(Livro)
+admin.site.register(Leitura)
+admin.site.register(Nota)
+admin.site.register(Logado)
---
+++ Lista_de_Leitura/controle/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+
+class Pessoa(models.Model):
+ id_pessoa = models.AutoField(primary_key=True)
+ matricula = models.CharField(max_length=10)
+ senha = models.CharField(max_length=3)
+ curso = models.CharField(max_length=30)
+ def __str__(p):
+  return f"{p.matricula}: {p.curso}"
+
+class Livro(models.Model):
+ id_livro = models.AutoField(primary_key=True)
+ nome_livro = models.CharField(max_length=60)
+ nome_autor = models.CharField(max_length=50)
+ codigo_livro = models.CharField(max_length=80)
+ numero_paginas = models.IntegerField()
+ def __str__(l):
+  return f"{l.nome_livro}"
+
+class Leitura(models.Model):
+ id_leitura = models.AutoField(primary_key=True)
+ livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+ pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+ pagina_atual = models.IntegerField()
+ def __str__(L):
+  return f"Leitura de {L.pessoa.matricula} do Livro: {L.livro.nome_livro}"
+
+class Nota(models.Model):
+ id_nota = models.AutoField(primary_key=True)
+ leitura = models.ForeignKey(Leitura, on_delete=models.CASCADE,unique=True)
+ nota = models.IntegerField(choices=[(0,0),(1,1),(2,2),(3,3),(4,4),(5,5)])
+ def __str__(n):
+  return f"{n.id_nota}: {n.nota}"
+
+class Logado(models.Model):
+ id_login = models.AutoField(primary_key=True)
+ pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE,unique=True)
+ logado = models.BooleanField(choices=[(True,"sim"),(False,"não")])
+ def __str__(Log):
+  return f"Matrícula: {Log.pessoa.matricula}. Estado de  login: {Log.logado}"
+ 
