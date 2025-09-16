5a2ccc40168f844ecc558e6b0c2ff43d73d905e0
1JoaoPaulo1 <1joapaulo000@gmail.com>
2025-09-16 16:47:41 -0300

    ligações entra as partes do site
    Agora é possível navegar até certo ponto entre o site, e obter os valores dos livros adicionados para lista de leitura

--- Lista_de_Leitura/controle/views.py
+++ Lista_de_Leitura/controle/views.py
@@ -17,7 +17,7 @@ def login(request):
      online = Logado.objects.get(pessoa = p)
      online.logado = True
      online.save()
-     return render(request,"sites/Login.html")
+     return redirect("centro")
     else:
      l = Logado(pessoa = p,logado = False)
      l.save()
@@ -25,8 +25,7 @@ def login(request):
      online = Logado.objects.get(pessoa = p)
      online.logado = True
      online.save()     
-     return render(request,"sites/Login.html")
-
+     return redirect("centro")
      
    else:
     return render(request,"sites/Login.html")
@@ -51,4 +50,85 @@ def cadastro(request):
  else:
   return render(request,"sites/Cadastro.html")
 
- 
+
+def centro(request):
+ logado = Logado.objects.filter(logado=True)
+ if len(logado) == 1:
+  return render(request,"sites/Cantinho da Leitura.html")
+ else:
+  logado = Logado.objects.all()
+  for i in logado:
+   i.logado = False
+   i.save()
+  return render(request,"sites/Login.html")  
+
+def ajuda(request):
+ logado = Logado.objects.filter(logado=True)
+ if len(logado) == 1:
+  return render(request,"sites/Ajuda.html")
+ else:
+  logado = Logado.objects.all()
+  for i in logado:
+   i.logado = False
+   i.save()
+  return render(request,"sites/Login.html")
+
+
+def busca(request):
+ logado = Logado.objects.filter(logado=True)
+ if len(logado) == 1:
+  logado = Logado.objects.get(logado=True)
+  livro = Livro.objects.all()
+  return render(request,"sites/Busca.html",{"livro":livro})
+  
+ else:
+  logado = Logado.objects.all()
+  for i in logado:
+   i.logado = False
+   i.save()
+  return render(request,"sites/Login.html")
+
+
+def minha_lista(request):
+ logado = Logado.objects.filter(logado=True)
+ if len(logado) == 1:
+  if request.method == "GET":
+   logado = Logado.objects.get(logado=True)
+   leitura = Leitura.objects.filter(pessoa = logado.pessoa)
+  
+
+   return render(request,"sites/Minha Lista.html",{"leitura":leitura})
+  else:
+   if request.method =="POST":
+    valores = (request.POST)
+    leitura = Leitura.objects.get(id_leitura=valores["identificar"])
+    livro = Livro.objects.get(id_livro = leitura.livro.id_livro)
+    if int(valores["pagina_atual"]) >= 0 and int(valores["pagina_atual"]) <= livro.numero_paginas:
+     leitura.pagina_atual = valores["pagina_atual"]
+     leitura.save()
+     logado = Logado.objects.get(logado=True)
+     leitura = Leitura.objects.filter(pessoa = logado.pessoa)
+     return render(request,"sites/Minha Lista.html",{"leitura":leitura})
+    else:
+     logado = Logado.objects.filter(logado=True)
+     if len(logado) == 1:
+      if request.method == "GET":
+       logado = Logado.objects.get(logado=True)
+       leitura = Leitura.objects.filter(pessoa = logado.pessoa)
+       return render(request,"sites/Minha Lista.html",{"leitura":leitura})
+     else:
+      logado = Logado.objects.all()
+      for i in logado:
+       i.logado = False
+       i.save()
+      return render(request,"sites/Login.html")
+
+
+
+ else:
+  logado = Logado.objects.all()
+  for i in logado:
+   i.logado = False
+   i.save()
+  return render(request,"sites/Login.html")
+
